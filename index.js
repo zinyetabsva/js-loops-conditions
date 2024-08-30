cbf746475ca71972485be32598cda26b0139cf50
zinyetabsva <innaiglesias3@gmail.com>
2024-08-30 23:57:31 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let num1 ="*";
+let num2 ="**";
+let num3 ="***";
+let num4 ="****";
+let num5 ="*****";
+let num6 ="******";
+let num7 ="*******";
+let num8 ="********";
+let num9 ="*********";
+let num10 ="**********";
+
+console.log(num1,num2,num3,num4,num5,num6,num7,num8,num9,num10);{
+}
+
+    
